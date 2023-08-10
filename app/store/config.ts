34599ff857bc104dfcbc3330e03045f220828373
lookfrank <129953124+lookfrank@users.noreply.github.com>
2023-08-10 12:54:40 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -59,6 +59,7 @@ export const ALL_MODELS = [
     name: "gpt-4",
     available: ENABLE_GPT4,
   },
+  /*
   {
     name: "gpt-4-0314",
     available: ENABLE_GPT4,
@@ -67,10 +68,12 @@ export const ALL_MODELS = [
     name: "gpt-4-0613",
     available: ENABLE_GPT4,
   },
+  */
   {
     name: "gpt-4-32k",
     available: ENABLE_GPT4,
   },
+  /*
   {
     name: "gpt-4-32k-0314",
     available: ENABLE_GPT4,
@@ -79,14 +82,17 @@ export const ALL_MODELS = [
     name: "gpt-4-32k-0613",
     available: ENABLE_GPT4,
   },
+  */
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
+  /*
   {
     name: "gpt-3.5-turbo-0301",
     available: true,
   },
+  */
   {
     name: "gpt-3.5-turbo-0613",
     available: true,
@@ -99,6 +105,7 @@ export const ALL_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
+  /*
   {
     name: "qwen-v1", // 通义千问
     available: false,
@@ -111,13 +118,14 @@ export const ALL_MODELS = [
     name: "spark", // 讯飞星火
     available: false,
   },
+  */
   {
-    name: "llama", // llama
-    available: false,
+    name: "联网GPT", // 联网GPT
+    available: true,
   },
   {
-    name: "chatglm", // chatglm-6b
-    available: false,
+    name: "URL读取", // 读URL
+    available: true,
   },
 ] as const;
 
